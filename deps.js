eeebb95fbafa7ffc616b566dd9981ee567761b90
PRoutamaa <89683328+PRoutamaa@users.noreply.github.com>
2024-11-14 10:42:50 +0200

    Update deps.js

--- deps.js
+++ deps.js
@@ -3,9 +3,9 @@ export {
   Application,
   Router,
   send,
-} from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import postgres from "https://deno.land/x/postgresjs@v3.4.2/mod.js";
+} from "https://deno.land/x/oak@v17.1.3/mod.ts";
+import postgres from "https://deno.land/x/postgresjs/mod.js";
 export { postgres };
-export { Session } from "https://deno.land/x/oak_sessions@v4.1.9/mod.ts";
+export { Session } from "https://deno.land/x/oak_sessions@v9.0.0/mod.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
 export * as validasaur from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
